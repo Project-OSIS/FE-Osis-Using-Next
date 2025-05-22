6c8a39b8180fc4ba4445935cedcabec0cc0fab79
grayhonz <zulfiqar.ridho@gmail.com>
2025-05-22 11:35:23 +0700

    push login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 // src/app/page.tsx
-import Header from './components/Header';
-import HeroSection from './components/HeroSection';
-import TrendingCollections from './components/TrendingCollection';
-import TopCreators from './components/TopCreators';
-import Categories from './components/Categories';
-import DiscoverMoreNFTs from './components/DiscoverMoreNFTs';
-import HowItWorks from './components/HowItWorks';
-import JoinWeeklyDigest from './components/JoinWeeklyDigest';
-import Footer from './components/Footer';
+import Header from '../components/Header';
+import HeroSection from '../components/HeroSection';
+import TrendingCollections from '../components/TrendingCollection';
+import TopCreators from '../components/TopCreators';
+import Categories from '../components/Categories';
+import DiscoverMoreNFTs from '../components/DiscoverMoreNFTs';
+import HowItWorks from '../components/HowItWorks';
+import JoinWeeklyDigest from '../components/JoinWeeklyDigest';
+import Footer from '../components/Footer';
+
 
 export default function Home() {
   return (
